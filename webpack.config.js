eb59feb53ce3b62e512193413bbb3602cf0c2940
Nat Welch <nat@natwelch.com>
2019-06-30 21:20:47 +0000

    cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,20 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: "babel-loader",
+          },
+          {
+            loader: "react-svg-loader",
+            options: {
+              jsx: true, // true outputs JSX tags
+            },
+          },
+        ],
+      },
     ],
   },
   externals: {

9cfec0cf4c76b4b3e730be0a669306e04f488fd9
Nat Welch <nat@natwelch.com>
2019-06-30 19:07:59 +0000

    fix publish

--- publish.sh
+++ publish.sh
@@ -4,5 +4,5 @@ OLDVERSION=$(jq -r ".version" package.json | sed 's/^.*-//')
 VERSION=$(date +%Y.%m.%d)-$(($OLDVERSION + 1))
 jq ".version = \"$VERSION\"" package.json | sponge package.json
 git commit package.json -m "$VERSION"
-git tag -s -a $VERSION
-git push -a
+git tag -s $VERSION -m $VERSION
+git push --all

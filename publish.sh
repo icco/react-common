75cd43902a91e044c185f3a8c25cb8b689c4378b
Nat Welch <nat@natwelch.com>
2019-07-01 00:21:48 +0000

    try new version

--- publish.sh
+++ publish.sh
@@ -5,9 +5,20 @@ set -ex
 git diff --exit-code --shortstat
 git push --all
 
-OLDVERSION=$(jq -r ".version" package.json | sed 's/^.*-//')
-VERSION=$(date +%Y.%m.%d | sed 's/\.0/./')-$(($OLDVERSION + 1))
-jq ".version = \"$VERSION\"" package.json | sponge package.json
-git commit package.json -m "$VERSION"
-git tag -s $VERSION -m $VERSION
-git push --tags
+DATE=$(date +%Y.%m.%d | sed 's/\.0/./g')
+OLDVERSION=$(jq -r ".version" package.json)
+OLDINT=$(echo $OLDVERSION | sed 's/^.*-//')
+OLDDATE=$(echo $OLDVERSION | sed 's/-.*$//')
+
+INT=1
+if [[ ${DATE} -eq ${OLDDATE} ]]; then
+  INT=$(($OLDINT + 1))
+fi
+
+VERSION=${DATE}-${INT}
+echo $VERSION
+
+#jq ".version = \"$VERSION\"" package.json | sponge package.json
+#git commit package.json -m "$VERSION"
+#git tag -s $VERSION -m $VERSION
+#git push --tags

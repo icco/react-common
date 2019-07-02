05b74335623d6a526db580f818ef89b0053c09a6
Nat Welch <nat@natwelch.com>
2019-07-02 02:04:44 +0000

    logo

--- src/lib/Logo.js
+++ src/lib/Logo.js
@@ -1,11 +1,30 @@
 import React from "react";
 import SVG from "svg.js";
 
-function buildSvg(size) {
-  let root = SVG().size(size, size)
-  return root.circle(size/4).stroke("#000")
-}
+function buildSVG(size, el) {
+  var canvas = SVG(el).size(size, size);
+  var k = size / 4;
+  [[k * 1, k * 1], [k * 3, k * 1], [k * 1, k * 3], [k * 3, k * 3]].forEach(
+    function(arr, i) {
+      // Set the radius
+      var c = canvas.circle(size / 4.0 + size / 10.0);
 
+      // Put it where we want to
+      c.cx(arr[0]);
+      c.cy(arr[1]);
+
+      // Style it
+      c.fill({ color: "#fff", opacity: 0.0 });
+      c.stroke({
+        width: 0.04 * size,
+        color: "#000",
+      });
+    }
+  );
+
+  //new Vivus('drawing', { duration: 200 }, myCallback);
+  return el;
+}
 
 class Logo extends React.Component {
   render() {
@@ -14,13 +33,7 @@ class Logo extends React.Component {
       size = this.props.size;
     }
 
-    let svg = buildSvg(size);
-
-    return (
-      <div className={this.props.className} >
-        {svg}
-      </div>
-    );
+    return buildSVG(size, <div className={this.props.className} />);
   }
 }
 

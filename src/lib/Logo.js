ea9a0df83e426e538e18ea7efa065b23b0573e2f
Nat Welch <nat@natwelch.com>
2019-07-17 23:56:15 +0000

    lint

--- src/lib/Logo.js
+++ src/lib/Logo.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { SVG } from "@svgdotjs/svg.js";
 import "@svgdotjs/svg.topath.js";
-import Vivus from "vivus"
+import Vivus from "vivus";
 
 // This modifies the DOM, as such, can only be called from componentDidMount.
 function buildSVG(size, el) {
-  let canvas = SVG().addTo(el).size(size, size);
-  canvas.id("logo")
+  let canvas = SVG()
+    .addTo(el)
+    .size(size, size);
+  canvas.id("logo");
   let k = size / 4;
   [[k * 1, k * 1], [k * 3, k * 1], [k * 1, k * 3], [k * 3, k * 3]].forEach(
     function(arr, i) {

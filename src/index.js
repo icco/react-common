823aa5430a63f1f3d9e9a7afceef45f656bd7ec9
Nat Welch <nat@natwelch.com>
2019-07-20 23:52:54 +0000

    new middleware

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import ErrorMessage from "./lib/ErrorMessage";
 import Header from "./lib/Header";
 import Loading from "./lib/Loading";
 import Logo from "./lib/Logo";
+import SSLMiddleware from "./lib/SSLMiddleware";
 
-export { ErrorMessage, Header, Loading, Logo };
+export { ErrorMessage, Header, Loading, Logo, SSLMiddleware };
---
+++ src/lib/SSLMiddleware.js
@@ -0,0 +1,15 @@
+module.exports = function() {
+  environments = ['production'];
+  status = 302;
+  return function(req, res, next) {
+    if (environments.indexOf(process.env.NODE_ENV) >= 0) {
+      if (req.get('x-forwarded-proto') != 'https' && req.path != "/healthz") {
+        res.redirect(status, `https://${req.hostname}${req.originalUrl}`);
+      } else {
+        next();
+      }
+    } else {
+      next();
+    }
+  };
+};

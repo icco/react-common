3efc2290141b6ff04b8af847ab154fb0899eafb4
Nat Welch <nat@natwelch.com>
2019-07-21 00:04:29 +0000

    not on localhost

--- src/lib/SSLMiddleware.js
+++ src/lib/SSLMiddleware.js
@@ -3,7 +3,11 @@ module.exports = function() {
   status = 302;
   return function(req, res, next) {
     if (environments.indexOf(process.env.NODE_ENV) >= 0) {
-      if (req.get("x-forwarded-proto") != "https" && req.path != "/healthz") {
+      if (
+        req.get("x-forwarded-proto") != "https" &&
+        req.path != "/healthz" &&
+        req.host != "localhost"
+      ) {
         res.redirect(status, `https://${req.hostname}${req.originalUrl}`);
       } else {
         next();

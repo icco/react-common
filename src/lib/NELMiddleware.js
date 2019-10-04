0dbd4699659e72a17d5de1b2517a7dd998de313a
Nat Welch <nat@natwelch.com>
2019-10-04 03:14:13 +0000

    new middleware

---
+++ src/lib/NELMiddleware.js
@@ -0,0 +1,10 @@
+
+module.exports = function(app) {
+  return function(req, res, next) {
+        res.setHeader(
+          "NEL",
+          JSON.stringify({ report_to: "default", max_age: 2592000 })
+        );
+        next();
+  }
+}
---
+++ src/lib/ReportToMiddleware.js
@@ -0,0 +1,17 @@
+module.exports = function(app) {
+  return function(req, res, next) {
+        res.setHeader(
+          "Report-To",
+          JSON.stringify({
+            group: "default",
+            max_age: 10886400,
+            endpoints: [
+              {
+                url: `https://reportd.natwelch.com/report/${app}`,
+              },
+            ],
+          })
+        );
+        next();
+  }
+}

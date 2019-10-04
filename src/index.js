74f3b0e7866d733f66de406b0dbb60e0aea0c364
Nat Welch <nat@natwelch.com>
2019-10-04 03:15:34 +0000

    add to index

--- src/index.js
+++ src/index.js
@@ -2,6 +2,16 @@ import ErrorMessage from "./lib/ErrorMessage";
 import Header from "./lib/Header";
 import Loading from "./lib/Loading";
 import Logo from "./lib/Logo";
+import NELMiddleware from "./lib/NELMiddleware";
+import ReportToMiddleware from "./lib/ReportToMiddleware";
 import SSLMiddleware from "./lib/SSLMiddleware";
 
-export { ErrorMessage, Header, Loading, Logo, SSLMiddleware };
+export {
+  ErrorMessage,
+  Header,
+  Loading,
+  Logo,
+  NELMiddleware,
+  ReportToMiddleware,
+  SSLMiddleware
+};

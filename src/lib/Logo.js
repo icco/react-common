ce7c40e65a2754103c1be92b6716a459f3e207af
Nat Welch <nat@natwelch.com>
2019-12-08 00:45:38 +0000

    upgrade ref usage

--- src/lib/Logo.js
+++ src/lib/Logo.js
@@ -10,27 +10,35 @@ function buildSVG(size, el) {
     .size(size, size);
   canvas.id("logo");
   let k = size / 4;
-  [[k * 1, k * 1], [k * 3, k * 1], [k * 1, k * 3], [k * 3, k * 3]].forEach(
-    function(arr, i) {
-      // Set the radius
-      let c = canvas.circle(size / 4.0 + size / 10.0);
+  [
+    [k * 1, k * 1],
+    [k * 3, k * 1],
+    [k * 1, k * 3],
+    [k * 3, k * 3],
+  ].forEach(function(arr, i) {
+    // Set the radius
+    let c = canvas.circle(size / 4.0 + size / 10.0);
 
-      // Put it where we want to
-      c.cx(arr[0]);
-      c.cy(arr[1]);
+    // Put it where we want to
+    c.cx(arr[0]);
+    c.cy(arr[1]);
 
-      // Style it
-      c.fill("none");
-      c.stroke({
-        width: 0.04 * size,
-        color: "#000",
-      });
-      let path = c.toPath();
-    }
-  );
+    // Style it
+    c.fill("none");
+    c.stroke({
+      width: 0.04 * size,
+      color: "#000",
+    });
+    let path = c.toPath();
+  });
 }
 
 class Logo extends React.Component {
+  constructor(props) {
+    super(props);
+    this.myRef = React.createRef();
+  }
+
   componentDidMount() {
     const { size } = this.props;
     buildSVG(size, this.refs.svg);
@@ -44,7 +52,7 @@ class Logo extends React.Component {
       <div
         style={{ width: `${size}px`, height: `${size}px` }}
         className={this.props.className}
-        ref="svg"
+        ref={this.myRef}
       />
     );
   }

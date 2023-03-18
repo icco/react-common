0d7e8b1fcd06aa6e6a4c71bdf944e6a8957720cf
Nat Welch <nat@natwelch.com>
2023-03-18 22:07:39 +0000

    lint

--- src/lib/Nord.js
+++ src/lib/Nord.js
@@ -27,7 +27,87 @@ export default {
   nord15: "#b48ead",
 };
 
-
-    export const theme = {"gradients":{},"durations":["250ms"],"boxShadows":{"0":"","1":"0px 10px 20px 2px rgba(184, 194, 215, 0.35)","2":"0px 3px 6px 0px rgba(15, 17, 21, 0.2)"},"description":"A port of the Nord theme.","borderStyles":[],"letterSpacings":{"tracked":"0.1em"},"styles":{},"text":{},"borderWidths":["1px"],"lineHeights":{"1-5":1.45},"colors":{"night":["#2e3440","#394050","#454e61","#4c566a"],"snow":["#eceff4","#e5e9f0","#d8dee9"],"frost":["#8FBCBB","#88C0D0","#81A1C1","#5E81AC"],"aurora":["#BF616A","#D08770","#EBCB8B","#B48EAD","#A3BE8C"],"modes":{"dark":{"background":"#394050","text":"#eceff4","border":"#e5e9f0","primary":"#88C0D0","accent":"#4c566a","muted":"#d8dee9"},"light":{"background":"#eceff4","text":"#2e3440","border":"#454e61","primary":"#88C0D0","accent":"#d8dee9","muted":"#394050"},"colorful":{"background":"#B48EAD","text":"#eceff4","border":"#2e3440","primary":"#2e3440","accent":"#d8dee9","muted":"#e5e9f0"}}},"breakpoints":["30em","60em"],"fonts":{"heading":"Work Sans","body":"Quattrocento Sans","mono":"Roboto Mono"},"borderRadius":["8px"],"textShadows":{},"space":["16px","32px","48px","64px","80px","96px","112px","128px","144px"],"name":"Nat Nord","fontSizes":["12px","13px","14px","15px","16px","18px","20px","23px","24px","29px","32px","36px","41px"],"easingFunctions":{"0":"steps(0, jump-end)"}}
-
-
+export const theme = {
+  gradients: {},
+  durations: ["250ms"],
+  boxShadows: {
+    0: "",
+    1: "0px 10px 20px 2px rgba(184, 194, 215, 0.35)",
+    2: "0px 3px 6px 0px rgba(15, 17, 21, 0.2)",
+  },
+  description: "A port of the Nord theme.",
+  borderStyles: [],
+  letterSpacings: { tracked: "0.1em" },
+  styles: {},
+  text: {},
+  borderWidths: ["1px"],
+  lineHeights: { "1-5": 1.45 },
+  colors: {
+    night: ["#2e3440", "#394050", "#454e61", "#4c566a"],
+    snow: ["#eceff4", "#e5e9f0", "#d8dee9"],
+    frost: ["#8FBCBB", "#88C0D0", "#81A1C1", "#5E81AC"],
+    aurora: ["#BF616A", "#D08770", "#EBCB8B", "#B48EAD", "#A3BE8C"],
+    modes: {
+      dark: {
+        background: "#394050",
+        text: "#eceff4",
+        border: "#e5e9f0",
+        primary: "#88C0D0",
+        accent: "#4c566a",
+        muted: "#d8dee9",
+      },
+      light: {
+        background: "#eceff4",
+        text: "#2e3440",
+        border: "#454e61",
+        primary: "#88C0D0",
+        accent: "#d8dee9",
+        muted: "#394050",
+      },
+      colorful: {
+        background: "#B48EAD",
+        text: "#eceff4",
+        border: "#2e3440",
+        primary: "#2e3440",
+        accent: "#d8dee9",
+        muted: "#e5e9f0",
+      },
+    },
+  },
+  breakpoints: ["30em", "60em"],
+  fonts: {
+    heading: "Work Sans",
+    body: "Quattrocento Sans",
+    mono: "Roboto Mono",
+  },
+  borderRadius: ["8px"],
+  textShadows: {},
+  space: [
+    "16px",
+    "32px",
+    "48px",
+    "64px",
+    "80px",
+    "96px",
+    "112px",
+    "128px",
+    "144px",
+  ],
+  name: "Nat Nord",
+  fontSizes: [
+    "12px",
+    "13px",
+    "14px",
+    "15px",
+    "16px",
+    "18px",
+    "20px",
+    "23px",
+    "24px",
+    "29px",
+    "32px",
+    "36px",
+    "41px",
+  ],
+  easingFunctions: { 0: "steps(0, jump-end)" },
+};
